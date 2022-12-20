75d780449d0a9cba9c3f47f67ce620b541c8a664
IshanMK <e18173@eng.pdn.ac.lk>
2022-12-20 23:15:09 +0530

    Deployment done on vercel

--- Web Application/frontend/src/pages/index.js
+++ Web Application/frontend/src/pages/index.js
@@ -1,16 +1,16 @@
 import Head from "next/head";
 import { Box, Container, Grid, Button } from "@mui/material";
-import { Budget } from "./components/dashboard/budget";
-import { LatestOrders } from "./components/dashboard/latest-orders";
-import { LatestProducts } from "./components/dashboard/latest-products";
-import { Sales } from "./components/dashboard/sales";
-import { TasksProgress } from "./components/dashboard/tasks-progress";
-import { TotalCustomers } from "./components/dashboard/total-customers";
-import { TotalProfit } from "./components/dashboard/total-profit";
-import { TrafficByDevice } from "./components/dashboard/traffic-by-device";
-import { DashboardLayout } from "./components/dashboard-layout";
-import Map from "./pages/map";
-import { Google as GoogleIcon } from "./icons/google";
+import { Budget } from "../components/dashboard/budget";
+import { LatestOrders } from "../components/dashboard/latest-orders";
+import { LatestProducts } from "../components/dashboard/latest-products";
+import { Sales } from "../components/dashboard/sales";
+import { TasksProgress } from "../components/dashboard/tasks-progress";
+import { TotalCustomers } from "../components/dashboard/total-customers";
+import { TotalProfit } from "../components/dashboard/total-profit";
+import { TrafficByDevice } from "../components/dashboard/traffic-by-device";
+import { DashboardLayout } from "../components/dashboard-layout";
+import Map from "../pages/map";
+import { Google as GoogleIcon } from "../icons/google";
 import WarningIcon from "@mui/icons-material/Warning";
 import CallIcon from "@mui/icons-material/Call";
 
