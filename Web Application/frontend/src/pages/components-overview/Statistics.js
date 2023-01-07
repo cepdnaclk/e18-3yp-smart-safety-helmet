c9ae2884b83419cd600bb156ee1f4dcfda45902a
IshanMK <e18173@eng.pdn.ac.lk>
2023-01-07 18:36:02 +0530

    All the pages requires authentication

--- Web Application/frontend/src/pages/components-overview/Statistics.js
+++ Web Application/frontend/src/pages/components-overview/Statistics.js
@@ -1,9 +1,55 @@
 import { Box, Container, Grid } from '@mui/material';
 import { statistics } from '../../__mocks__/statistics';
 import { StatCard } from '../../components/statistics/stat-card';
+import { useEffect } from 'react';
 
-const Page = () => (
-    <>
+//import assests
+import { useNavigate } from 'react-router-dom';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
+// firebase app configuration
+const firebaseConfig = {
+    apiKey: 'AIzaSyBK0t0b6M_dS7Jin7D7dgGCeKbZq_dq5FQ',
+    authDomain: 'smart-helmet-74616.firebaseapp.com',
+    databaseURL: 'https://smart-helmet-74616-default-rtdb.firebaseio.com',
+    projectId: 'smart-helmet-74616',
+    storageBucket: 'smart-helmet-74616.appspot.com',
+    messagingSenderId: '20313702925',
+    appId: '1:20313702925:web:e293f804b8bbaaa6018b44'
+};
+
+const Page = () => {
+    // Navigator
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        async function getData() {
+            //initialize the App
+            firebase.initializeApp(firebaseConfig);
+
+            //checks whether a user is successfully logged in or not
+            firebase.auth().onAuthStateChanged((user) => {
+                if (user) {
+                    //if there is logged user already
+                    //navigate to dashboard
+                    navigate('/statistics');
+                    // ...
+                } else {
+                    // User is signed out
+                    // No user is signed in.
+                    // console.log('No User Signed In');
+
+                    //if there is no logged user go to login
+                    navigate('/login');
+                }
+            });
+        }
+
+        getData();
+    }, []);
+
+    return (
         <Box
             component="main"
             sx={{
@@ -33,7 +79,7 @@ const Page = () => (
         </Box> */}
             </Container>
         </Box>
-    </>
-);
+    );
+};
 
 export default Page;
