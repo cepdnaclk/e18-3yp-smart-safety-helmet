467d329d54d218802d6146fc1fd561fc70e7e676
IshanMK <e18173@eng.pdn.ac.lk>
2023-01-07 20:04:35 +0530

    Fixed a bug in menu items

--- Web Application/frontend/src/menu-items/index.js
+++ Web Application/frontend/src/menu-items/index.js
@@ -2,7 +2,6 @@
 import pages from './pages';
 import dashboard from './dashboard';
 import utilities from './utilities';
-import support from './support';
 
 // ==============================|| MENU ITEMS ||============================== //
 
